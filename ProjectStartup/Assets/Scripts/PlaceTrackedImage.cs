5cffc978b54f74362aacf142ad10def8177a99b3
DariaCristiana24 <dariacristiana24@gmail.com>
2023-11-23 12:51:23 +0100

    changes to my scene and added the project that works with img tracking

---
+++ ProjectStartup/Assets/Scripts/PlaceTrackedImage.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaceTrackedImage : MonoBehaviour
+{
+    [SerializeField]
+    ARTrackedImageManager m_TrackedImageManager;
+    [SerializeField]
+    GameObject prefab;
+    [SerializeField]
+    GameObject prefabToDestroy;
+
+    void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
+
+    void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var newImage in eventArgs.added)
+        {
+            // Handle added event
+            Debug.Log("image added");
+            Instantiate(prefab);
+            Destroy(prefabToDestroy);
+        }
+
+        foreach (var updatedImage in eventArgs.updated)
+        {
+            // Handle updated event
+        }
+
+        foreach (var removedImage in eventArgs.removed)
+        {
+            // Handle removed event
+        }
+    }
+
+    private void Update()
+    {
+       ListAllImages();
+       // Debug.Log(m_TrackedImageManager.referenceLibrary.count);
+    }
+
+    void ListAllImages()
+    {
+        Debug.Log(
+            $"There are {m_TrackedImageManager.trackables.count} images being tracked.");
+
+        foreach (var trackedImage in m_TrackedImageManager.trackables)
+        {
+            Debug.Log($"Image: {trackedImage.referenceImage.name} is at " +
+                      $"{trackedImage.transform.position}");
+        }
+    }
+}
