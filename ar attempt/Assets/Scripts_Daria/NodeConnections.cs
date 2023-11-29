781689b7699130dbe8fe2e70695920a9f914455f
RianneTV <138168573+RianneTV@users.noreply.github.com>
2023-11-29 15:34:09 +0100

    start of VisualisePathManager

---
+++ ar attempt/Assets/Scripts_Daria/NodeConnections.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeConnections : MonoBehaviour
+{
+    public Node nodeA;
+    public Node nodeB;
+    public bool activePath = false;
+}
